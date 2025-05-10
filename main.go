fecb1a14f9f8d4503dacced73558c7184dfe1229
Jacob Pitkin <jacob.pitkin.78@gmail.com>
2025-05-09 18:07:21 -0700

    trying to connect to MariaDB and do some transactions for learning. MariaDB can't connect to root at the moment, need to try something else.

--- main.go
+++ main.go
@@ -1,14 +1,103 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+	db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/root?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 
-	http.ListenAndServe(":8080", nil)
+	{
+		query := `
+		CREATE TABLE users (
+			id INT AUTO_INCREMENT,
+			username TEXT NOT NULL,
+			password TEXT NOT NULL,
+			created_at DATETIME,
+			PRIMARY KEY (id)
+		);`
+
+		if _, err := db.Exec(query); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	{
+		username := "Jacob"
+		password := "secret"
+		createdAt := time.Now()
+
+		result, err := db.Exec(`INSERT INTO users(username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, _ := result.LastInsertId()
+		fmt.Println(id)
+	}
+
+	{
+		var (
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		)
+
+		query := "SELECT id, username, password, created_at FROM users WHERE id = ?"
+		if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(id, username, password, createdAt)
+	}
+
+	{
+		type user struct {
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		}
+
+		rows, err := db.Query(`SELECT id, username, password, createdAt FROM users`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var users []user
+		for rows.Next() {
+			var u user
+
+			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%#v", users)
+	}
+
+	{
+		_, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
